9d67ba237badcf828dc755d5b2ecc49745ecc4a4
opilane <opilane@HP-CZC6297B8L>
2021-11-24 11:26:12 +0200

    .

--- module1.py
+++ module1.py
@@ -1,16 +1,35 @@
-pasword = ''.join([random.choice(ls) for x in range(12)])
-return psword 
-return psword
-def paskontroll(psword: str)->bool:
-	ls=list(psword)
-	for e in ls:
-		if e.isdigit(): d=True
-		if e.isalpha(): a=True
-		if e.isupper(): u=True
-		if e.islower():l=True
-		if e in list(".,:;!_*-+()/#¤%&"): s=True
-	if d==True and a==True and u==True and l==True and s==True:
-		t=True
+import random 
+while True:
+	print("Reg-1,Avt-2,Välja-3")
+	v=int(input())
+	if v==1:
+		print("Registreerimine")
+		while 1:
+			log=input("Kasutajatunnus")
+			if log not in users:
+				print("Tunnus soobib")
+				break
+			else:
+				print("See nimi juba on olemas listis")	
+		v=input("Arvuti-A või ise-I loob parool")
+		if v.upper()=="A":
+			pas=passautomat()
+		elif v.upper()=="I":			
+			while 1:
+				pas=input("Sisesta oma parool")
+				tulemus=paskontroll(pas)
+				if tulemus==True:
+					users.append(log)
+					passwords.append(pas)
+					break
+
+
+	elif v==2:
+		print("Avtoriseerimine")
+		#
+	elif v==3:
+		print("Välja")
+		break
+		#valmis
 	else:
-		t=False
-	return t 
+		print("On vaja valida 1,2 või 3")# kõik on olemas
